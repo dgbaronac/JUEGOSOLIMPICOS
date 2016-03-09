bb7c7eec4653faee41df9b5c29536d1119b2178d
Felipe <Felipe@FEELIIPEE>
2016-03-08 20:44:33 -0500

    new

---
+++ src/juegosolimpicos/Grupo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegosolimpicos;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Grupo {
+    private String nDeporte;
+
+    public Grupo(String nDeporte) {
+        this.nDeporte = nDeporte;
+    }
+
+    public String getnDeporte() {
+        return nDeporte;
+    }
+
+    public void setnDeporte(String nDeporte) {
+        this.nDeporte = nDeporte;
+    }
+    
+    
+}
