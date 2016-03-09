4d78f05989ed181871e9d09590238df256d2a3ed
dgbaronac <dgbaronac@unal.edu.co>
2016-03-09 15:22:11 -0500

    Update Integrante.java

--- src/juegosolimpicos/Integrante.java
+++ src/juegosolimpicos/Integrante.java
@@ -7,8 +7,66 @@ package juegosolimpicos;
 
 /**
  *
- * @author Felipe
+ * @author David
  */
-public class Integrante {
+import java.util.*;
+
+public class Delegacion {
+    
+    
+    private String Pais;
+    private int numDeportistas;
+    private int medOro;
+    private int medPlata;
+    private int medBronce;
+
+    public Delegacion(String Pais, int numDeportistas, int medOro, int medPlata, int medBronce) {
+        this.Pais = Pais;
+        this.numDeportistas = numDeportistas;
+        this.medOro = medOro;
+        this.medPlata = medPlata;
+        this.medBronce = medBronce;
+    }
+    
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String Pais) {
+        this.Pais = Pais;
+    }
+
+    public int getNumDeportistas() {
+        return numDeportistas;
+    }
+
+    public void setNumDeportistas(int numDeportistas) {
+        this.numDeportistas = numDeportistas;
+    }
+
+    public int getMedOro() {
+        return medOro;
+    }
+
+    public void setMedOro(int medOro) {
+        this.medOro = medOro;
+    }
+
+    public int getMedPlata() {
+        return medPlata;
+    }
+
+    public void setMedPlata(int medPlata) {
+        this.medPlata = medPlata;
+    }
+
+    public int getMedBronce() {
+        return medBronce;
+    }
+
+    public void setMedBronce(int medBronce) {
+        this.medBronce = medBronce;
+    }
+    
     
 }
